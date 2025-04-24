569167e53493c8c0c33066dc32dcf19c97928774
GABRIEL QUEIROZ MARQUES <25170172@educ123.sp.senai.br>
2025-04-24 11:14:26 -0300

    Adicionando

--- funcoes/moldura.js
+++ funcoes/moldura.js
@@ -1,2 +1,20 @@
 //desenha uma modlura em frase
-const readline = require(readline-sync);
+const readline = require('readline-sync');
+const ch = '█'
+
+function barra(qtde){
+    for (let i =0;i<qtde+4;i++)
+    {
+        process.stdout.write(ch)
+    }
+    console.log()
+}
+const frase = readline.question("Digite a frase: ");
+
+barra(frase.length);
+barra(frase.length);
+
+console.log(`${ch} ${frase} ${ch}`)
+
+barra(frase.length);
+barra(frase.length);
